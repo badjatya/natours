6e01762b6561a232cc27f7c4ebb7e18ae61b3196
Archit Badjatya <en19cs304010@medicaps.ac.in>
2021-11-12 16:41:26 +0530

    Updating and deleting tour from database

--- src/controllers/tourController.js
+++ src/controllers/tourController.js
@@ -58,21 +58,42 @@ const getTour = async (req, res) => {
   }
 };
 
-function updateTour(req, res) {
-  res.status(200).json({
-    status: "success",
-    data: {
-      tour: "<Updated tour here...>",
-    },
-  });
-}
+const updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      runValidators: true,
+      new: true,
+    });
 
-function deleteTour(req, res) {
-  res.status(204).json({
-    status: "success",
-    data: null,
-  });
-}
+    res.status(200).json({
+      status: "success",
+      data: {
+        tour,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: "fail",
+      message: error,
+    });
+  }
+};
+
+const deleteTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndDelete(req.params.id);
+
+    res.status(204).json({
+      status: "success",
+      data: null,
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: "fail",
+      message: error,
+    });
+  }
+};
 
 module.exports = {
   getTours,

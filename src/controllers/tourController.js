5961e8e09acfe8ee58cb2b745311d0bf20ed25a3
Archit Badjatya <en19cs304010@medicaps.ac.in>
2021-11-14 18:58:46 +0530

    Refactored the app

--- src/controllers/tourController.js
+++ src/controllers/tourController.js
@@ -1,6 +1,9 @@
 // Models
 const Tour = require("../models/tour");
 
+// Utils
+const APIFeatures = require("../utils/apiFeatures");
+
 // Alias route for top 5 best tour
 const aliasTopTours = (req, res, next) => {
   req.query.limit = "5";
@@ -12,50 +15,13 @@ const aliasTopTours = (req, res, next) => {
 
 const getTours = async (req, res) => {
   try {
-    // 1A) FILTERING
-    const queryObject = { ...req.query };
-    const excludedFields = ["page", "sort", "limit", "fields"];
-    excludedFields.forEach((el) => delete queryObject[el]);
-
-    // 1B) ADVANCED FILTERING
-    let queryString = JSON.stringify(queryObject);
-    queryString = queryString.replace(
-      /\b(gte|gt|lte|lt)\b/g,
-      (match) => `$${match}`
-    );
-
-    let query = Tour.find(JSON.parse(queryString));
-
-    // 2) SORTING
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(",").join(" ");
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort("-createdAt");
-    }
-
-    // 3) FIELDS LIMITING
-    if (req.query.fields) {
-      const fields = req.query.fields.split(",").join(" ");
-      query = query.select(fields);
-    } else {
-      query = query.select("-__v");
-    }
-
-    // 4) PAGINATION
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-
-    query = query.skip(skip).limit(limit);
-
-    if (req.query.page) {
-      const numTours = await Tour.countDocuments();
-      if (skip >= numTours) throw new Error("This page does not exists");
-    }
-
     // Executing
-    const tours = await query;
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const tours = await features.query;
 
     res.status(200).json({
       status: "success",

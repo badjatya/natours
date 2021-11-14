52f53684070909dd66d05408a9791fb4ac38dbbb
Archit Badjatya <en19cs304010@medicaps.ac.in>
2021-11-14 14:23:43 +0530

    Limiting fileds

--- src/controllers/tourController.js
+++ src/controllers/tourController.js
@@ -25,6 +25,14 @@ const getTours = async (req, res) => {
       query = query.sort("-createdAt");
     }
 
+    // 3) FIELDS LIMITING
+    if (req.query.fields) {
+      const fields = req.query.fields.split(",").join(" ");
+      query = query.select(fields);
+    } else {
+      query = query.select("-__v");
+    }
+
     // Executing
     const tours = await query;
 

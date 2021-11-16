24623ebf974b5c46c8a4c19fdd4be084298f0e2e
Archit Badjatya <en19cs304010@medicaps.ac.in>
2021-11-16 13:26:56 +0530

    Aggreagation

--- src/routes/tourRoutes.js
+++ src/routes/tourRoutes.js
@@ -9,11 +9,13 @@ const {
   deleteTour,
   aliasTopTours,
   getTourStats,
+  getMonthlyPlan,
 } = require("../controllers/tourController");
 
 // Top 5 cheap aliasing to getRoutes
 router.route("/top-5-cheap").get(aliasTopTours, getTours);
 router.route("/tour-stats").get(getTourStats);
+router.route("/monthly-plan/:year").get(getMonthlyPlan);
 
 router.route("/").get(getTours).post(createTour);
 router.route("/:id").get(getTour).patch(updateTour).delete(deleteTour);

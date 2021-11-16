92c7a980e9d8f69e1ab3cda106981984e9165e99
Archit Badjatya <en19cs304010@medicaps.ac.in>
2021-11-16 09:03:15 +0530

    Aggregation

--- src/controllers/tourController.js
+++ src/controllers/tourController.js
@@ -113,6 +113,45 @@ const deleteTour = async (req, res) => {
   }
 };
 
+const getTourStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingsAverage: { $gte: 4.5 } },
+      },
+      {
+        $group: {
+          _id: { $toUpper: "$difficulty" },
+          numTours: { $sum: 1 },
+          numRating: { $sum: "$ratingsQuantity" },
+          avgRating: { $avg: "$ratingsAverage" },
+          avgPrice: { $avg: "$price" },
+          minPrice: { $min: "$price" },
+          maxPrice: { $max: "$price" },
+        },
+      },
+      {
+        $sort: {
+          avgPrice: 1,
+        },
+      },
+    ]);
+
+    // RESPONSE
+    res.status(200).json({
+      status: "success",
+      data: {
+        stats,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: "fail",
+      message: error,
+    });
+  }
+};
+
 module.exports = {
   getTours,
   getTour,
@@ -120,4 +159,5 @@ module.exports = {
   updateTour,
   deleteTour,
   aliasTopTours,
+  getTourStats,
 };
--- src/routes/tourRoutes.js
+++ src/routes/tourRoutes.js
@@ -8,10 +8,12 @@ const {
   updateTour,
   deleteTour,
   aliasTopTours,
+  getTourStats,
 } = require("../controllers/tourController");
 
 // Top 5 cheap aliasing to getRoutes
 router.route("/top-5-cheap").get(aliasTopTours, getTours);
+router.route("/tour-stats").get(getTourStats);
 
 router.route("/").get(getTours).post(createTour);
 router.route("/:id").get(getTour).patch(updateTour).delete(deleteTour);

d801a9364e6ece70aa6dd4c33332c20484d63a1d
Archit Badjatya <en19cs304010@medicaps.ac.in>
2021-11-18 08:37:59 +0530

    Sending errors

--- src/controllers/utils/errorController.js
+++ src/controllers/utils/errorController.js
@@ -1,9 +1,37 @@
+const sendDevelopmentErrorResponse = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    error: err,
+    stack: err.stack,
+  });
+};
+
+const sendProductionErrorResponse = (err, res) => {
+  // Trusted errors
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  }
+  // Programming errors
+  else {
+    console.error("ERROR ", err);
+    res.status(500).json({
+      status: "error",
+      message: "Something went wrong",
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === "development") {
+    sendDevelopmentErrorResponse(err, res);
+  } else if (process.env.NODE_ENV === "production") {
+    sendProductionErrorResponse(err, res);
+  }
 };

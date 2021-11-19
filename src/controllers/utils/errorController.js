4c07f2f8b3a76df1a8404af4001a0ad4913f7032
Archit Badjatya <en19cs304010@medicaps.ac.in>
2021-11-19 07:18:46 +0530

    sending production error

--- src/controllers/utils/errorController.js
+++ src/controllers/utils/errorController.js
@@ -1,3 +1,6 @@
+// Utils
+const AppError = require("../../utils/appError");
+
 const sendDevelopmentErrorResponse = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -25,6 +28,11 @@ const sendProductionErrorResponse = (err, res) => {
   }
 };
 
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
@@ -32,6 +40,11 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === "development") {
     sendDevelopmentErrorResponse(err, res);
   } else if (process.env.NODE_ENV === "production") {
-    sendProductionErrorResponse(err, res);
+    const error = { ...err };
+
+    if (error.name === "CastError") {
+      error = handleCastErrorDB(error, next);
+    }
+    sendProductionErrorResponse(error, res);
   }
 };
